d8b0a28420e7aaf5fff96bbce605e74eeaf3a100
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-03-27 23:25:41 +0300

    Did second task - made videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+const makeTimeUpdate = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+const throttledMakeTimeUpdate = throttle(makeTimeUpdate, 1000);
+
+player.on('timeupdate', throttledMakeTimeUpdate);
